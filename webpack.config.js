7c1f03e734246842857eb03bc9fe856d8558a1e0
Samuel Morales <moralessa978@gmail.com>
2022-05-01 18:48:20 -0600

    attempt to fix missing images

--- webpack.config.js
+++ webpack.config.js
@@ -57,6 +57,11 @@ module.exports = {
             test: /\.(png|svg|jpg|jpeg|gif)$/i,
             type: 'asset/resource',
           },
+          {
+            test: /\.html$/,
+            exclude: /node_modules/,
+            loader: 'html-loader'
+          },
         ]
     }
 }
